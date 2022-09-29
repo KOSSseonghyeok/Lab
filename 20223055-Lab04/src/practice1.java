368793b96787c95b678aded2f1e5fc290fb0e880
KOSSseonghyeok <104906731+KOSSseonghyeok@users.noreply.github.com>
2022-09-29 10:25:48 +0900

    Practice

---
+++ 20223055-Lab04/src/practice1.java
@@ -0,0 +1,10 @@
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		double probability = 8.70;
+		int percentage = (int) (100 * probability); //int percentage = (int) (10*Math.round(10*probability));로 수정하면 올바른 결과 출력
+		System.out.println(percentage); 
+	}
+
+}
